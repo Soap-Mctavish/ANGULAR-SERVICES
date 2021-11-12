84dbffeb1cb6d41f5e2dc325441e6d1d7d0184e2
Soap McTavish <86884247+Soap-Mctavish@users.noreply.github.com>
2021-11-12 23:58:52 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -3,21 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoDetailsComponent } from './todo-details/todo-details.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoserviceService } from './todoservice.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoDetailsComponent,
-    TodoListComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [TodoserviceService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
